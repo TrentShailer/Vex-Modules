a3b883fc78040ff468c702a93e0172423ccc11c7
Fantus <trent.shailer@gmail.com>
2021-07-27 11:26:32 +1200

    New Auton Select

--- AutonSelect.h
+++ AutonSelect.h
@@ -6,97 +6,55 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
+using std::vector;
 
 using namespace vex;
-
-side curSide = none;
-
-team curTeam = team::red;
-
 int timeForSelection = 15;
 
-static const char *EnumStrings[] = {"Left", "Right", "None"};
-static const char *EnumStringsTeam[] = {"Red", "Blue"};
+vector<vector<std::string>> options = {
+    {"Left", "None", "Right"}, {"Blue", "Red"}, {"Win Point", "No Win Point"}};
+vector<std::string> results = {};
 
-side InitAutonSelect()
-{
-	Brain.Screen.drawLine(160, 0, 160, 280);
-	Brain.Screen.drawLine(320, 0, 320, 280);
-	Brain.Screen.setCursor(6, 2);
-	Brain.Screen.print("Left");
-	Brain.Screen.setCursor(6, 22);
-	Brain.Screen.print("None");
-	Brain.Screen.setCursor(6, 44);
-	Brain.Screen.print("Right");
-	Brain.Screen.setCursor(0, 0);
+vector<std::string> Initialize() {
+  for (int i = 0; i < options.size(); i++) {
+    int numOfOptions = options[i].size();
 
-	for (int i = 0; i < (timeForSelection * 1000) / 20; i++)
-	{
-		int xPos = Brain.Screen.xPosition();
-		if (Brain.Screen.pressing())
-		{
-			if (xPos < 200)
-			{
-				curSide = side::left;
-			}
-			else if (xPos >= 160 && xPos < 320)
-			{
-				curSide = side::none;
-			}
-			else
-			{
-				curSide = side::right;
-			}
-			break;
-		}
-		wait(20, msec);
-	}
+    Brain.Screen.clearScreen();
 
-	Brain.Screen.clearScreen();
-	Brain.Screen.setCursor(6, 22);
-	Brain.Screen.print(EnumStrings[curSide]);
-	wait(1000, msec);
-	Brain.Screen.setCursor(0, 0);
-	Brain.Screen.clearScreen();
+    for (int j = 1; j <= numOfOptions; j++) {
+      int xPos = (480 / numOfOptions) * j;
 
-	return curSide;
-}
+      Brain.Screen.drawLine(xPos, 0, xPos, 280);
+      Brain.Screen.printAt(((480 / numOfOptions) / 2) +
+                               (480 / numOfOptions) * (j - 1) -
+                               (options[i][j - 1].length() * 6),
+                           130, options[i][j - 1].c_str());
+    }
 
-team InitTeamSelect()
-{
-	Brain.Screen.drawLine(240, 0, 240, 280);
-	Brain.Screen.setCursor(6, 2);
-	Brain.Screen.print("Blue");
-	Brain.Screen.setCursor(6, 44);
-	Brain.Screen.print("Red");
-	Brain.Screen.setCursor(0, 0);
+    for (int k = 0; k < (timeForSelection * 1000) / 20; k++) {
+      int pressPos = Brain.Screen.xPosition();
 
-	for (int i = 0; i < (timeForSelection * 1000) / 20; i++)
-	{
-		int xPos = Brain.Screen.xPosition();
-		if (Brain.Screen.pressing())
-		{
-			if (xPos < 240)
-			{
-				curTeam = team::blue;
-			}
-			else
-			{
-				curTeam = team::red;
-			}
-			break;
-		}
-		wait(20, msec);
-	}
+      if (Brain.Screen.pressing()) {
+        for (int l = 1; l <= numOfOptions; l++) {
 
-	Brain.Screen.clearScreen();
-	Brain.Screen.setCursor(6, 22);
-	Brain.Screen.print(EnumStringsTeam[curTeam]);
-	wait(1000, msec);
-	Brain.Screen.setCursor(0, 0);
-	Brain.Screen.clearScreen();
+          int xPos = (480 / numOfOptions) * l;
 
-	return curTeam;
+          if (pressPos < xPos) {
+            results.push_back(options[i][l - 1]);
+            Brain.Screen.clearScreen();
+            wait(500, msec);
+            break;
+          }
+        }
+        break;
+      }
+
+      wait(20, msec);
+    }
+  }
+  Brain.Screen.clearScreen();
+  return results;
 }
 
 #endif
